d6f0be2a27dee1298f4b7bb3c841929b9a95d4e1
awkwardkarrot <63611299+awkwardkarrot@users.noreply.github.com>
2020-09-29 02:04:12 -0700

    Update sketch.js
    other stuff

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+let cartoons = [{name:"grimadventure", character: "mandy"}, {name:"batmanbeyond", character: "terry"}, {name:"6teen", character:"jude"}, "ededdneddy"];
+
 function setup() {
   createCanvas(400, 400);
+  background(220);
+
+console.log(cartoons[0].character);
+
+console.log(random(2));
+console.log (cartoons[int(random(2))]);
+
+  cartoons.splice();
+  console.log("array after splice");
+  console.log(cartoons);
+
+  cartoons.unshift("starvstheforcesofevil");
+  console.log("array after unshift");
+  console.log(cartoons);
+
+
 }
 
 function draw() {
-  background(220);
+
 }
