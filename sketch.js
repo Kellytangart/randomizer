d5a9f2f59596a21ca2d24528714b0f0f680c743a
awkwardkarrot <63611299+awkwardkarrot@users.noreply.github.com>
2020-10-01 02:48:50 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,58 @@
-let cartoons = [{name:"grimadventure", character: "mandy"}, {name:"batmanbeyond", character: "terry"}, {name:"6teen", character:"jude"}, "ededdneddy"];
+let cartoons = [{
+  name:"grimadventure",
+  character: "mandy"
+}, {
+  name:"batmanbeyond",
+  character: "terry"
+}, {
+  name:"6teen",
+  character:"jude"
+}, {
+  name:"ededdneddy",
+  character: "kevin"
+}];
+
+
+let randomIndex;
+let counter =0;
+let animating= false;
 
 function setup() {
-  createCanvas(400, 400);
-  background(220);
-
-console.log(cartoons[0].character);
-
-console.log(random(2));
-console.log (cartoons[int(random(2))]);
-
-  cartoons.splice();
-  console.log("array after splice");
-  console.log(cartoons);
-
-  cartoons.unshift("starvstheforcesofevil");
-  console.log("array after unshift");
-  console.log(cartoons);
+  createCanvas(600, 600);
+  background(200);
+  textSize(32);
+text("click to randomize", 50,50);
 
 
 }
 
 function draw() {
+  if (animating == true){
+    ellipse(random(width), random(height), random(50,200));
+  }
+}
+
+
+
+function randomizer(){
+  animating= false;
+  if (cartoons[0]) {
+  //this displays random name and splices it out of array
+  background(random(200, 255));
+  randomIndex= int(random(cartoons.length));
+  text(cartoons[randomIndex].name + " has a character called "
+   + cartoons[randomIndex]
+    .character, 50, 50);
+  cartoons.splice(randomIndex, 1);
+} else {
+  background(random(200, 255));
+  text("nothing left", 50, 50);
+  }
+
+}
+
+function mousePressed(){
+  animating= true;
+setTimeout(randomizer, 2000);
 
 }
