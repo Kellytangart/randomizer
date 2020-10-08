35fb5b381666dda9a8f8cc23fe7e35bff9d20bc8
awkwardkarrot <63611299+awkwardkarrot@users.noreply.github.com>
2020-10-07 23:49:29 -0700

    tu

--- sketch.js
+++ sketch.js
@@ -16,20 +16,45 @@ let cartoons = [{
 let randomIndex;
 let counter =0;
 let animating= false;
+let tickets= [];
+let imageCounter= 0;
+let button;
+
+function preload(){
+  for (let i=0; i <=7; i++){
+    tickets[i] = loadImage("assets/ticket_"  + i + ".jpg")
+  }
+}
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(800, 800);
   background(200);
   textSize(32);
-text("click to randomize", 50,50);
+  textAlign(CENTER);
+  textStyle(BOLD);
+  imageMode(CENTER);
+  frameRate(8);
 
+text("click to randomize", 50,50);
+//console.log(tickets);
+
+button = createButton("click to randomize");
+button.mousePressed(buttonPressed);
 
 }
 
 function draw() {
   if (animating == true){
-    ellipse(random(width), random(height), random(50,200));
-  }
+    clear();
+  image(tickets[imageCounter], width/2 , height/2 );
+
+  if (imageCounter < tickets.length - 1){
+  imageCounter++;
+  console.log(imageCounter);
+} else{
+  imageCounter= 0;
+ }
+}
 }
 
 
@@ -38,20 +63,21 @@ function randomizer(){
   animating= false;
   if (cartoons[0]) {
   //this displays random name and splices it out of array
-  background(random(200, 255));
+  //background(random(200, 255));
+  clear();
   randomIndex= int(random(cartoons.length));
-  text(cartoons[randomIndex].name + " has a character called "
-   + cartoons[randomIndex]
+  text(cartoons[randomIndex].name + cartoons[randomIndex]
     .character, 50, 50);
+    image(random(tickets), width/2 , height/2 );
   cartoons.splice(randomIndex, 1);
-} else {
-  background(random(200, 255));
-  text("nothing left", 50, 50);
+//} else {
+  //background(random(200, 255));
+  //text("nothing left", 50, 50);
   }
 
 }
 
-function mousePressed(){
+function buttonPressed(){
   animating= true;
 setTimeout(randomizer, 2000);
 
